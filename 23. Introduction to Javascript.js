33cfdb790ddc8207077ec1519ba917e49a715bd6
DebaR200 <h.22.debashis@gmail.com>
2025-10-05 23:23:07 +0530

    Create 23. Introduction to Javascript.js

---
+++ 23. Introduction to Javascript.js
@@ -0,0 +1,19 @@
+alert("hello World");
+
+console.log("Code is running...");
+console.log("Code is also running");
+console.log("Code is looking like a wow");
+
+var a = prompt("Enter a number");
+console.log("The number is " + a);
+
+var isTrue = confirm("Do you want to leave this page");
+if(isTrue){
+    console.log("yes please");
+}
+else{
+    console.log("No, won't");
+}
+
+document.title = "Hey I am good";
+document.body.style.backgroundColor ="red";
