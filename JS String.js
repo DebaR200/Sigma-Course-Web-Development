26a5c464c88195eadfc1fabd6aaa9868119b797f
DebaR200 <h.22.debashis@gmail.com>
2025-10-07 23:09:20 +0530

    Create JS String.js

---
+++ JS String.js
@@ -0,0 +1,34 @@
+console.log("This is tutorial on String")
+
+let name = "Harry"
+
+console.log(name)
+console.log(name.length)
+
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+
+let real_name = "Hero Alum"
+let friend_name = "puppy bhai"
+console.log("My name is " + real_name + " and my friend name is " + friend_name);
+console.log(`His name is ${real_name} and his friends name is ${friend_name}`)
+
+let name1 = " Harry "
+console.log(name1.trim())
+// Remove white spaces
+
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+
+console.log(name.slice(1,5))
+console.log(name.slice(2))
+
+let a ="Sneha"
+let b ="Saha"
+console.log(a.concat(b, "Gadha"))
+
+let c = "Sneha"
+console.log(c.replace("Sn", "Bn"))
